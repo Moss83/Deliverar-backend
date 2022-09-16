660cc03abdef77ad505b6bc6b0f4b0fba664fea3
Mauricio Mosquera <mauriciomos@hotmail.com>
2022-09-16 20:11:06 -0300

    initial commit

---
+++ src/main/java/com/example/adtpoapi/DeliverarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.adtpoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adtpoapi/DeliverarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adtpoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
