2ed2dd328adf3c80aae1c5d1be53f2d6993e5198
Mauricio Mosquera <mauriciomos@hotmail.com>
2022-11-23 21:11:56 -0300

    esdfsfd

--- src/main/java/com/example/adtpoapi/model/MensajeFranquicia.java
+++ src/main/java/com/example/adtpoapi/model/MensajeFranquicia.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "mensajesFranquicia")
+@Table(name = "mensajesfranquicia")
 public class MensajeFranquicia {
 
 	@Id
